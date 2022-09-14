30844730c56d82c70f851c685adad718a1b7c7d6
VanessaLazatin <lazatinvanessa@gmail.com>
2022-09-14 14:53:59 +0200

    rs

---
+++ src/components/SliderData.js
@@ -0,0 +1,26 @@
+export const SliderData = [
+  {
+    image:
+      "https://images.unsplash.com/photo-1660735839772-fe0dc14012a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDJ8NnNNVmpUTFNrZVF8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=60",
+  },
+
+  {
+    image:
+      "https://images.unsplash.com/photo-1662913497683-0934ae1958b9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDZ8NnNNVmpUTFNrZVF8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=60",
+  },
+
+  {
+    image:
+      "https://images.unsplash.com/photo-1662627015683-1bb877c0808c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDE2fDZzTVZqVExTa2VRfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+  },
+
+  {
+    image:
+      "https://images.unsplash.com/photo-1662522195455-956b870bad32?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDQ5fDZzTVZqVExTa2VRfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+  },
+  
+  {
+    image:
+      "https://images.unsplash.com/photo-1661030259336-6fae4251997c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDExOHw2c01WalRMU2tlUXx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60",
+  },
+];
